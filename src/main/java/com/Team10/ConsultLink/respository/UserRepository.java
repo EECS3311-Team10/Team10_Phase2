49d5643287580e0f20c1a43e765ee87ce98d9e59
AronJoseph-21 <ajo21@my.yorku.ca>
2026-03-29 20:33:56 -0400

    Added User Repository

---
+++ src/main/java/com/Team10/ConsultLink/respository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Team10.ConsultLink.respository;
+
+import com.Team10.ConsultLink.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    // This magic method will automatically write the SQL to find a user by name
+    User findByName(String name);
+}
