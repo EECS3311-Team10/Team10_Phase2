7439880a53d081d01688ec6794e52ac3e247b624
AronJoseph-21 <ajo21@my.yorku.ca>
2026-04-04 17:29:38 -0400

    Update DatabaseConfig.java

--- src/main/java/com/Team10/ConsultLink/config/DatabaseConfig.java
+++ src/main/java/com/Team10/ConsultLink/config/DatabaseConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.Team10.ConsultLink.respository.UserRepository;
+import com.Team10.ConsultLink.repository.UserRepository;
 import com.Team10.ConsultLink.users.Admin;
 import com.Team10.ConsultLink.users.User;
 
