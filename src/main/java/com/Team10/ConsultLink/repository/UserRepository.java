b806e33e24358b2c8e8dca70e168c51e16e1365f
AronJoseph-21 <ajo21@my.yorku.ca>
2026-04-04 21:04:25 -0400

    Update UserRepository.java

--- src/main/java/com/Team10/ConsultLink/repository/UserRepository.java
+++ src/main/java/com/Team10/ConsultLink/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // This magic method will automatically write the SQL to find a user by name
-    User findByName(String name);
+    User findByUserId(String userId);
 }
