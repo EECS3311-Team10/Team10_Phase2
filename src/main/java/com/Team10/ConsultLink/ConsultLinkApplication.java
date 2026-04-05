068f4d454c74486978fd2c5f56572e625d8ea6d4
AronJoseph-21 <ajo21@my.yorku.ca>
2026-04-04 21:04:07 -0400

    Update ConsultLinkApplication.java

--- src/main/java/com/Team10/ConsultLink/ConsultLinkApplication.java
+++ src/main/java/com/Team10/ConsultLink/ConsultLinkApplication.java
@@ -1,16 +1,14 @@
 package com.Team10.ConsultLink;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; // Add this import
 
-
-
-@SpringBootApplication
+// The 'exclude' tells Spring to ignore the auto-generated login screen
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) 
 public class ConsultLinkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsultLinkApplication.class, args);
-
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultLinkApplication.class, args);
+    }
 }
