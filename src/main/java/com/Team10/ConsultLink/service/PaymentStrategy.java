e18b4bcd911a30e456f712a3d2f099052ea42040
AronJoseph-21 <ajo21@my.yorku.ca>
2026-04-04 21:04:49 -0400

    Update PaymentStrategy.java

--- src/main/java/com/Team10/ConsultLink/service/PaymentStrategy.java
+++ src/main/java/com/Team10/ConsultLink/service/PaymentStrategy.java
@@ -2,7 +2,7 @@ package com.Team10.ConsultLink.service;
 import com.Team10.ConsultLink.model.*;
 
 public interface PaymentStrategy {
-	public void pay();
+	public void pay(double amount);
 	public PaymentMethod getPaymentMethod();
 	public void simPaymentProcessing();
 }
