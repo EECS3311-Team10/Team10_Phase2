c7efe7f845e056a4a2082c3f66ae954aad95df69
jingchun-ji <jingchun@my.yorku.ca>
2026-04-05 23:54:21 -0400

    delete comments

--- src/main/java/com/Team10/ConsultLink/model/PaymentMethod.java
+++ src/main/java/com/Team10/ConsultLink/model/PaymentMethod.java
@@ -23,30 +23,3 @@ public abstract class PaymentMethod {
     public String getPaymentType() { return paymentType; }
     public void setPaymentType(String paymentType) { this.paymentType = paymentType; }
 }
-
-/*package com.Team10.ConsultLink.model;
-
-public abstract class PaymentMethod {
-    private static int idCounter = 1; // static counter for generating unique payment method IDs
-    protected int paymentMethodId;
-    protected String paymentType;
-    // Constructor
-    public PaymentMethod(String paymentType) {
-        this.paymentMethodId = idCounter++;
-        this.paymentType = paymentType;
-    }
-
-    // Getters for payment method ID and type
-    public int getPaymentMethodId() {
-        return paymentMethodId;
-    }
-
-    public String getPaymentType() {
-        return paymentType;
-    }
-
-    public abstract boolean validate();
-    public abstract String getPaymentDetails();
-
-}
-*/
